ff2090b04078969e5d164d627d906cae3ce9980e
Ulysse Carion <ulyssecarion@gmail.com>
2014-10-07 00:51:19 -0700

    Only consider 10,000 most significant words for sentiment.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,17 @@
 import nltk.classify.util
-from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
+from nltk.classify import NaiveBayesClassifier
+from nltk.probability import FreqDist, ConditionalFreqDist
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+
+# Only the `num_words_considered` most informative words are chosen to be
+# considered when finding a phrase's sentiment value.
+#
+# Higher numbers means you take into account more words, but also means that
+# some of the noisier, less informative, words are going to be included in your
+# conclusions.
+num_words_considered = 10000
 
 # What percent of the data will we use for training? The rest will be used for
 # evaluating the accuracy of our model.
@@ -32,10 +43,49 @@ def train_classifier(feature_fn):
 
     return classifier, nltk.classify.util.accuracy(classifier, test_features)
 
-def word_feats(words):
-    return dict([(word, True) for word in words])
+# A frequency distribution of how often words appear.
+word_freq_dist = FreqDist()
 
-classifier, accuracy = train_classifier(word_feats)
+# A frequency distribution of how often words are associated with 'pos' or
+# 'neg'.
+label_word_freq_dist = ConditionalFreqDist()
 
+# Build up the frequency distributions.
+for label in ['pos', 'neg']:
+    for word in movie_reviews.words(categories = [label]):
+        word_freq_dist[word.lower()] += 1
+        label_word_freq_dist[label][word.lower()] += 1
+
+num_pos_words = label_word_freq_dist['pos'].N()
+num_neg_words = label_word_freq_dist['neg'].N()
+total_num_words = num_pos_words + num_neg_words
+
+def word_information_score(word, freq):
+    pos_score = BigramAssocMeasures.chi_sq(
+        label_word_freq_dist['pos'][word],
+        (freq, num_pos_words),
+        total_num_words)
+
+    neg_score = BigramAssocMeasures.chi_sq(
+        label_word_freq_dist['neg'][word],
+        (freq, num_neg_words),
+        total_num_words)
+
+    return pos_score + neg_score
+
+word_info_scores = {word: word_information_score(word, freq)
+    for word, freq in word_freq_dist.items()}
+
+sorted_by_score = sorted(word_info_scores.items(),
+    key = lambda word_score_pair: word_score_pair[1],
+    reverse = True)[:num_words_considered]
+
+best_words = set([word for word, score in sorted_by_score])
+
+def best_word_feats(words):
+    return dict([(word, True) for word in words if word in best_words])
+
+print('Classifying based on presence of most informative words')
+classifier, accuracy = train_classifier(best_word_feats)
 print('Estimated accuracy: {}'.format(accuracy))
 classifier.show_most_informative_features()
